b7f3eb563ca685193e84eeadacff132b01d7a2f9
matias gonzalez <matias5859@gmail.com>
2022-09-04 19:51:52 -0300

    primero

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const servicios= [
+   { id: 1, nombre: "vaquillonas braford", precio: 550.000,img: "braford.png.png"},
+   { id: 2, nombre: "vaquillonas bragus coloradas ", precio: 550.000,img: "brangus-coloradas.png"},
+   { id: 3, nombre: "vaquillonas brangus overas", precio: 550.000,img: "bragus-negro.png.png"},
+   { id: 4, nombre: "yeguas de polo", precio: 800.000,img: "caballosBOX.png.png"},
+   { id: 5, nombre: "vaquillonas criollas", precio: 450.000,img: "criollas.png"},
+   { id: 6, nombre: "vaquillonas holandas", precio: 650.000,img: "lecheras.png"},
+   { id: 7, nombre: "vaquillonas shorthorn", precio: 850.000,img: "shorthorn.png"},
+   { id: 8, nombre: "clases de polo", precio: 2.500,img: "polo-bochapiso.png.png"},
+
+    
+]
